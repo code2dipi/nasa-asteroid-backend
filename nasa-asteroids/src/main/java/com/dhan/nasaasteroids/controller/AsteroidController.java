9bc4bdcd201d15242d88182d3f4d50d2f987f89f
Dipi <dipineupane@gmail.com>
2023-12-13 02:41:25 +0100

    Recoverd lost files Asteroid controller

--- nasa-asteroids/src/main/java/com/dhan/nasaasteroids/controller/AsteroidController.java
+++ nasa-asteroids/src/main/java/com/dhan/nasaasteroids/controller/AsteroidController.java
@@ -1,2 +1,66 @@
-package com.dhan.nasaasteroids.controller;public class AsteroidController {
+package com.dhan.nasaasteroids.controller;
+
+import com.dhan.nasaasteroids.exception.NotFoundException;
+import com.dhan.nasaasteroids.exception.UnauthorizedException;
+import com.dhan.nasaasteroids.model.NeoObject;
+import com.dhan.nasaasteroids.service.AsteroidService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/asteroids")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AsteroidController {
+    private static final Logger logger = LoggerFactory.getLogger(AsteroidController.class);
+
+    @Autowired
+    private AsteroidService asteroidService;
+
+    @GetMapping("/landing")
+    public ResponseEntity<List<NeoObject>> getAsteroidsForLandingPage(
+            @RequestParam String startDate,
+            @RequestParam String endDate,
+            @RequestParam String apiKey) {
+        logger.info("Received request for asteroids landing page. Start Date: {}, End Date: {}", startDate, endDate);
+        try {
+            LocalDate parsedStartDate = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
+            LocalDate parsedEndDate = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
+
+            List<NeoObject> asteroids = asteroidService.getAsteroidsForLandingPage(parsedStartDate, parsedEndDate, apiKey);
+
+            return ResponseEntity.ok(asteroids);
+        } catch (IllegalArgumentException e) {
+            logger.error("Bad request for landing page. Error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        } catch (UnauthorizedException e) {
+            logger.error("Unauthorized request for landing page. Error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
+
+    @GetMapping("/details/{asteroidId}")
+    public ResponseEntity<NeoObject> getAsteroidDetails(
+            @PathVariable String asteroidId,
+            @RequestParam String apiKey) {
+        try {
+            NeoObject asteroid = asteroidService.getAsteroidDetails(asteroidId, apiKey);
+            logger.info("Asteroid detials: " + asteroid);
+
+            return ResponseEntity.ok(asteroid);
+        } catch (NotFoundException e) {
+            logger.error("Asteroid not found. Error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } catch (UnauthorizedException e) {
+            logger.error("Unauthorized request for asteroid details. Error: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
+        }
+    }
 }
--- nasa-asteroids/src/test/java/com/dhan/nasaasteroids/service/AsteroidServiceTest.java
+++ nasa-asteroids/src/test/java/com/dhan/nasaasteroids/service/AsteroidServiceTest.java
@@ -1,4 +1,39 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.dhan.nasaasteroids.service;
+
+import com.dhan.nasaasteroids.controller.AsteroidController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.client.RestTemplate;
+
+
+@ExtendWith(MockitoExtension.class)
 class AsteroidServiceTest {
-  
+    private static final Logger logger = LoggerFactory.getLogger(AsteroidServiceTest.class);
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks
+    private AsteroidService asteroidService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void TestGetAsteroidsForLandingPage() {
+
+    }
+
+    @Test
+    void testGetAsteroidDetails() {
+    }
 }
